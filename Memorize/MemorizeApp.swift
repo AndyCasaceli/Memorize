c4728d9ddf3e08972c59f4ce47b6adcabfd0a366
Andy Casaceli <acasaceli@Andys-MacBook-Pro.local>
2020-12-01 17:00:37 -0500

    Starting project

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Andy Casaceli on 12/1/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
